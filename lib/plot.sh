1db3a399a0be93efe2cbb173ea621a6a523f068d
shanshan950 <39412563+shanshan950@users.noreply.github.com>
2021-09-02 10:56:35 -0400

    Update plot.sh

--- lib/plot.sh
+++ lib/plot.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-chr=$1
-start=$2
-end=$3
+anchor_bed=$1
+DeepLoop_output_dir=$2
+chr=$3
+start=$4
+end=$5
 
-./generate.raw.HiCorr.DeepLoop.matrix.pl anchor_bed/$chr.bed HK2662/$chr.raw_HiCorr_DeepLoop $chr $start $end ./Plots/HK2662.{$chr}_${start}_${end} &
-./generate.raw.HiCorr.DeepLoop.matrix.pl anchor_bed/$chr.bed HK2663/$chr.raw_HiCorr_DeepLoop $chr $start $end ./Plots/HK2663.{$chr}_${start}_${end} &
-./generate.raw.HiCorr.DeepLoop.matrix.pl anchor_bed/$chr.bed merge/$chr.raw_HiCorr_DeepLoop $chr $start $end ./Plots/merge.{$chr}_${start}_${end} &
-wait
-for file in raw.matrix HiCorr.matrix DeepLoop.matrix;do
-        ./plot.heatmap.r Plots/HK2662.{$chr}_${start}_${end}.${file}
-	./plot.heatmap.r Plots/HK2663.{$chr}_${start}_${end}.${file}
-	./plot.heatmap.r Plots/merge.{$chr}_${start}_${end}.${file}
-done
+./generate.matrix.by.DeepLoop.pl $anchor_bed/$chr.bed $DeepLoop_output_dir/$chr.denoised.anchor.to.anchor $chr $start $end {$chr}_${start}_${end} 
+./plot.heatmap.r {$chr}_${start}_${end}.DeepLoop.matrix
+
