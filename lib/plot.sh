6bbb4ae9db5b9ff159b9dcd392543a295bbe2e93
JinLab <53835021+JinLabBioinfo@users.noreply.github.com>
2021-10-31 14:22:26 -0400

    Add files via upload

--- lib/plot.sh
+++ lib/plot.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-anchor_bed=$1
-DeepLoop_output_dir=$2
-chr=$3
-start=$4
-end=$5
+DeepLoopInstallPath=$1
+DeepLoopRefbed=$2
+HiCorr_path=$3
+DeepLoop_outPath=$4
+chr=$5
+start=$6
+end=$7
+outPath=$8
 
-./generate.matrix.by.DeepLoop.pl $anchor_bed/$chr.bed $DeepLoop_output_dir/$chr.denoised.anchor.to.anchor $chr $start $end {$chr}_${start}_${end} 
-./plot.heatmap.r {$chr}_${start}_${end}.DeepLoop.matrix
+$DeepLoopInstallPath/DeepLoop/lib/generate.matrix.from_HiCorr.pl $DeepLoopRefbed/$chr.bed $HiCorr_path/anchor_2_anchor.loop.$chr $chr $start $end $outPath/${chr}_${start}_${end}
+$DeepLoopInstallPath/DeepLoop/lib/generate.matrix.from_DeepLoop.pl $DeepLoopRefbed/$chr.bed $DeepLoop_outPath/$chr.denoised.anchor.to.anchor $chr $start $end $outPath/${chr}_${start}_${end}
+$DeepLoopInstallPath/DeepLoop/lib/plot.multiple.r $outPath/${chr}_${start}_${end} 1 3 $outPath/${chr}_${start}_${end}.raw.matrix $outPath/${chr}_${start}_${end}.ratio.matrix $outPath/${chr}_${start}_${end}.denoise.matrix
 
